5441615346f3b02b0c7eda60da789c7384088bab
paduvireh <paduvireh@unjbg.edu.pe>
2024-07-08 11:13:52 -0500

    Se agrego registros1

---
+++ registros1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct empleado{
+	string nombre;
+	char sexo;
+	float sueldo;
+};
+
+int main(){
+	int n;
+	cout<<"Ingrese el numero de empleados: "; cin>>n;
+	empleado E[n];
+	
+	for(int i=0; i<n; i++){
+		cout<<"Ingrese el nombre del empleado "<<i+1<<": ";	
+		cin.ignore();
+		getline(cin, E[i].nombre);
+	
+		cout<<"Ingrese el sexo del empleado "<<i+1<<" (M/F): ";
+		cin>>E[i].sexo;
+	
+		cout<<"Ingrese el sueldo del empleado "<<i+1<<": ";
+		cin>>E[i].sueldo;
+
+	}
+	return 0;
+}
